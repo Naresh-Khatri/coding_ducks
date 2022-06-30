e1e4502bb00ea886f4bb06fe77ee14a8f1e0acf3
naresh-khatri <naresh.khatri2345@gmail.com>
2022-06-30 23:51:12 +0530

    add route guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,15 @@
-import { route } from 'quasar/wrappers'
-import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
-import routes from './routes'
+import { route } from "quasar/wrappers";
+import {
+  createRouter,
+  createMemoryHistory,
+  createWebHistory,
+  createWebHashHistory,
+} from "vue-router";
+import routes from "./routes";
+
+import { Notify } from "quasar";
+
+import { authServices } from "src/boot/firebase.js";
 
 /*
  * If not building with SSR mode, you can
@@ -14,7 +23,9 @@ import routes from './routes'
 export default route(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
+    : process.env.VUE_ROUTER_MODE === "history"
+    ? createWebHistory
+    : createWebHashHistory;
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
@@ -23,8 +34,41 @@ export default route(function (/* { store, ssrContext } */) {
     // Leave this as is and make changes in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
-    history: createHistory(process.env.VUE_ROUTER_BASE)
-  })
+    history: createHistory(process.env.VUE_ROUTER_BASE),
+  });
+  Router.beforeEach(async (to, from) => {
+    try {
+      const user = await authServices.getCurrentUser();
+      // console.log(user);
+      // console.log(from, to);
+      // console.log(
+      //   user,
+      //   to.matched.some((record) => record.meta.requiresAuth),
+      //   to.name !== "login"
+      // );
 
-  return Router
-})
+      // check if to route requires auth and avoid infinite loop
+      if (
+        to.matched.some((record) => record.meta.requiresAuth) &&
+        to.name !== "login"
+      ) {
+        //to requires auth so check if user is logged in
+        if (Object.entries(user).length === 0) {
+          Notify.create({
+            message: "You must be logged in to access this page",
+            color: "negative",
+            position: "top",
+            timeout: 3000,
+          });
+
+          return Router.push({ name: "login" });
+        } else {
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  return Router;
+});

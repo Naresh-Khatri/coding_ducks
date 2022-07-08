c85839a5e965c74b724426c1e019ad0a7657a8ac
naresh-khatri <naresh.khatri2345@gmail.com>
2022-07-08 23:51:12 +0530

    core features done 😎

---
+++ src/pages/PlaygroundPage.vue
@@ -0,0 +1,147 @@
+<template>
+  <q-layout>
+    <q-page-container>
+      <q-page>
+        <q-splitter v-model="splitterModel" horizontal style="height: 100vh">
+          <template v-slot:before>
+            <div style="height: 100%">
+              <codemirror
+                v-model="code"
+                placeholder="Code goes here..."
+                class="full-width"
+                :style="{
+                  height: '100%',
+                  fontSize: '16px',
+                }"
+                :autofocus="true"
+                :indent-with-tab="true"
+                :tabSize="4"
+                :extensions="extensions"
+                @change="change"
+              />
+            </div>
+          </template>
+          <template v-slot:separator>
+            <q-avatar style="height: 50px" size="40px"> </q-avatar>
+          </template>
+          <template v-slot:after>
+            <div style="background: #282c34">
+              <div
+                class="text-center bg-white text-h6"
+                style="border-radius: 25px 25px 0px 0px"
+              >
+                <div class="q-py-sm flex flex-center">
+                  <div class="bar"></div>
+                </div>
+                Console:
+              </div>
+              <pre
+                class="q-pa-md bg-white"
+                style="font-size: 16px; margin: 0; font-family: Hack, monospace"
+                >{{ result.stdout || result.error }}</pre
+              >
+            </div>
+          </template>
+        </q-splitter>
+        <q-page-sticky position="bottom-right" :offset="[30, 30]">
+          <q-btn
+            round
+            size="lg"
+            icon="play_arrow"
+            color="positive"
+            @click="run"
+          />
+        </q-page-sticky>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref, computed, onMounted } from "vue";
+import { useQuasar } from "quasar";
+import { api } from "src/boot/axios";
+
+import Console from "src/components/OutputConsole.vue";
+
+import { Codemirror } from "vue-codemirror";
+import { python } from "@codemirror/lang-python";
+import { cpp } from "@codemirror/lang-cpp";
+import { oneDark } from "@codemirror/theme-one-dark";
+const extensions = [python(), oneDark];
+
+const splitterModel = ref(60);
+const code = ref("");
+const selectedTheme = ref("oneDark");
+const selectedLang = ref("python");
+
+const updateEditorKey = ref(0);
+const change = (e) => {
+  //save in localstorage
+  $q.localStorage.set(`savedPlayground`, e);
+};
+
+const $q = useQuasar();
+onMounted(() => {
+  //check for prev code in localstorage
+  loadSavedFile();
+  //hide dialog in dev server
+  if (window.location.port === "9000") return;
+  $q.dialog({
+    title: "Updates",
+    html: true,
+    message: `<ul>
+      <li>This IDE currently doesn't support interactive user input so
+      <b style='color:red'> dont use input() </b></li>
+      <li>Only python is supported</li>
+      <li>Code storage in your account will be added soon.</li>
+      </ul>`,
+  });
+});
+
+const loadSavedFile = () => {
+  const savedCode = $q.localStorage.getItem(`savedPlayground`);
+  if (savedCode) {
+    code.value = savedCode;
+    $q.notify({ message: "Previous session loaded 😎", color: "positive" });
+  } else code.value = "";
+  updateEditorKey.value++;
+};
+
+const updateSaveFile = () => {
+  const file = {
+    qNum: problemStore.currentProblem.id,
+    code: code.value,
+    lang: selectedLang.value,
+  };
+  $q.localStorage.set(`savedFile${$route.params.problemNum}`, file);
+};
+
+const result = ref("");
+const run = async () => {
+  try {
+    const res = await api.post("/playground", {
+      code: code.value,
+      lang: "python",
+    });
+    console.log(res.data);
+    result.value = res.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+</script>
+<style lang="scss" scoped>
+pre {
+  white-space: pre-wrap;
+}
+:deep(.q-splitter__separator) {
+  background-color: #282c34;
+}
+.bar {
+  background-color: #282c34;
+  border-radius: 5px;
+  width: 70px;
+  height: 10px;
+}
+</style>
